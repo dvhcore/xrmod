e68f5eb295b290a27fcaf0cea9448a721c38b7ce
DF4I <114317537+dn4i@users.noreply.github.com>
2025-09-16 03:25:16 +0800

    Create mnet.sh

---
+++ vpstool-script/mnet.sh
@@ -0,0 +1,30 @@
+#binbash
+
+#MENU
+wget -O /usr/bin/menu "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/MENU/menu.sh" && chmod +x /usr/bin/menu
+wget -O /usr/bin/menu-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/MENU/menu-vless.sh" && chmod +x /usr/bin/menu-vless
+wget -O /usr/bin/backupmenu "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/MENU/backupmenu.sh" && chmod +x /usr/bin/backupmenu
+
+#XRAY
+wget -O /usr/bin/add-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/XRAY/add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -O /usr/bin/del-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/XRAY/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -O /usr/bin/cek-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/XRAY/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+wget -O /usr/bin/renew-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/XRAY/renew-vless.sh" && chmod +x /usr/bin/renew-vless
+wget -O /usr/bin/user-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/XRAY/user-vless.sh" && chmod +x /usr/bin/user-vless
+wget -O /usr/bin/trial-vless "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/XRAY/trial-vless.sh" && chmod +x /usr/bin/trial-vless
+
+
+#OTHERS
+wget -O /usr/bin/limit "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/limit-speed.sh" && chmod +x /usr/bin/limit
+wget -O /usr/bin/add-host "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/add-host.sh" && chmod +x /usr/bin/add-host
+wget -O /usr/bin/cekport "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/cekport.sh" && chmod +x /usr/bin/cekport
+wget -O /usr/bin/certxray "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/certxray.sh" && chmod +x /usr/bin/certxray
+wget -O /usr/bin/dns "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/dns.sh" && chmod +x /usr/bin/dns
+wget -O /usr/bin/get-backres "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/get-backres.sh" && chmod +x /usr/bin/get-backres
+wget -O /usr/bin/restart "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/restart.sh" && chmod +x /usr/bin/restart
+wget -O /usr/bin/status "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/status.sh" && chmod +x /usr/bin/status
+wget -O /usr/bin/cleaner "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/logcleaner.sh" && chmod +x /usr/bin/cleaner
+wget -O /usr/bin/xp "https://raw.githubusercontent.com/vinstechmy/VlessWebsocket/main/OTHERS/xp.sh" && chmod +x /usr/bin/xp
+wget -O /usr/bin/nf "https://raw.githubusercontent.com/vinstechmy/MediaUnlockerTest/main/media.sh" && chmod +x /usr/bin/nf
+
+rm -rf mnet.sh
