c6c85a2c3b1d5e53d415d2144e250c0fd4357cea
DF4I <114317537+dn4i@users.noreply.github.com>
2025-12-18 22:42:14 +0800

    Update cek.sh

--- cleaner-script/cek.sh
+++ cleaner-script/cek.sh
@@ -4,20 +4,24 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 echo -n > /tmp/other.txt
-data=( `cat /usr/local/etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\\E[0;41;36m     XRAY Vless WS User Login      \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+data=($(cat /usr/local/etc/xray/config.json | grep '^###' | cut -d ' ' -f 2))
+echo -e "\033[0;34m══════════════════════════════════════════\033[0m"
+echo -e "\\E[0;44;37m      ⇱ XRAY Vless WS User Login ⇲        \E[0m"
+echo -e "\033[0;34m══════════════════════════════════════════\033[0m"
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
 echo -n > /tmp/ipvless.txt
-data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp:/g' | cut -d ":" -f 1 | sort | uniq`);
+# old command
+#data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
+data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 4 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp:/g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
+#old command
+#jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 4 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvless.txt
 else
@@ -31,12 +35,14 @@ if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
 jum2=$(cat /tmp/ipvless.txt | nl)
-echo "User : $akun";
+echo "user : $akun";
 echo "$jum2";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+echo -e "\033[0;34m══════════════════════════════════════════\e[m"
 fi
 rm -rf /tmp/ipvless.txt
 rm -rf /tmp/other.txt
 done
 echo ""
-
+read -n 1 -s -r -p "Press any key to back on menu xray"
+xraay
