185f11d2464704492dc618b5e16bf0fba1ef0d20
DF4I <114317537+dn4i@users.noreply.github.com>
2026-01-12 08:31:43 +0800

    Add script to configure network settings and rc.local

---
+++ cleaner-script/tw.sh
@@ -0,0 +1,57 @@
+#BinBash
+#Dyno
+
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# nano /etc/rc.local
+# By default this script does nothing.
+echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+systemctl restart netfilter-persistent
+exit 0
+END
+
+chmod +x /etc/rc.local
+
+systemctl enable rc-local
+
+systemctl restart rc-local.service
+
+clear
+
+cat > /etc/sysctl.conf <<-CT11
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+fs.file-max = 65535
+net.netfilter.nf_conntrack_max = 262144
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 30
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+fs.file-max = 51200
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+CT11
+
+sudo ln -s /etc/sysctl.conf /etc/sysctl.d/99-sysctl.conf
+
+clear
+
+reboot
